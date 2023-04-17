f9a4545301a880e187541e31a0d3aa471e3ce759
Lalita <lalita.alibay@hotmail.fr>
2023-04-17 14:45:05 +0000

    fix mistakes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGODB_URI);
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
